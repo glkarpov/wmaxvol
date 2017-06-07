28b9bd1b6393a93e33bcaa6d96315c4b5ef96819
glebsg <gleb.karpov@skolkovotech.ru>
2017-06-07 16:58:27 +0300

    swm updated

--- blocked_maxvol.py
+++ blocked_maxvol.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-def SWM(C, deriv, ind_x, ind_j):
+def SWM(C, deriv, i, j):
     B = np.copy(C)
-    tmp_columns = np.copy(B[:,ind_j:ind_j + deriv + 1])
-    tmp_columns[ind_j:ind_j + deriv + 1] -= np.eye(deriv + 1)
-    tmp_columns[ind_x:ind_x + deriv + 1] -= np.eye(deriv + 1)
+    tmp_columns = np.copy(B[:,j:j + deriv + 1])
+    tmp_columns[j:j + deriv + 1] -= np.eye(deriv + 1)
+    tmp_columns[i:i + deriv + 1] += np.eye(deriv + 1)
     
-    b = B[ind_x:ind_x + deriv + 1][:,ind_j:ind_j + deriv + 1]
+    b = B[i:i + deriv + 1][:,j:j + deriv + 1]
     
-    tmp_rows = np.copy(B[ind_x:ind_x + deriv + 1])
-    tmp_rows[:,ind_j:ind_j + deriv + 1] -= np.eye(deriv + 1)
+    tmp_rows = np.copy(B[i:i + deriv + 1])
+    tmp_rows[:,j:j + deriv + 1] -= np.eye(deriv + 1)
     
     B -= np.dot(tmp_columns, np.dot(np.linalg.inv(b),tmp_rows))
     return B
@@ -24,7 +24,7 @@ def mov_row(C, j, ind_x):
     C[j, :] = temp            
             
 
-def block_maxvol(A_init, tol=0.05, max_iters=100, swm_upd = True):
+def block_maxvol(A_init, tol=0.05, max_iters=100, swm_upd = True, debug = False):
 # work on parameters
     ids_init = A_init[:A_init.shape[1]]
     temp_init = np.dot(A_init,np.linalg.inv(ids_init))
@@ -41,7 +41,7 @@ def block_maxvol(A_init, tol=0.05, max_iters=100, swm_upd = True):
     iters = 0
 
     while Fl and (iters < max_iters) :
-        max_det = 0.99
+        max_det = 1.0
         for k in range(m,n,2):
                 pair = temp[k:k+2]
                 for j in range(0,m,2):
@@ -55,11 +55,18 @@ def block_maxvol(A_init, tol=0.05, max_iters=100, swm_upd = True):
             #Forming new permutation array
             form_permute(P,index[1],index[0])
             form_permute(P, index[1]+1, index[0]+1)
-
+            if debug == True:
+                print (P[:m])
+            if (swm_upd == True) and (debug == True): 
+                print('on the {} iteration with swm, pair {} {} chosen and pair{}'.format(iters,index[0],index[1],temp[index[0]:index[0]+2][:,index[1]:index[1]+2]))
+            if (swm_upd == False) and (debug == True):
+                print('on the {} iteration with stan.oper, pair {} {} chosenand pair{}'.format(iters,index[0],index[1],temp[index[0]:index[0]+2][:,index[1]:index[1]+2]))
             ### Recalculating with new rows position
             if swm_upd == True:
-                bljad = np.copy(temp)
-                temp = SWM(bljad,1,index[0],index[1])
+                blj = np.copy(temp)
+                temp = SWM(blj,1,index[0],index[1])
+                mov_row(A,index[1],index[0])
+                mov_row(A,index[1] + 1,index[0] + 1)
             else:    
                 mov_row(A,index[1],index[0])
                 mov_row(A,index[1] + 1,index[0] + 1)
@@ -69,4 +76,4 @@ def block_maxvol(A_init, tol=0.05, max_iters=100, swm_upd = True):
             iters += 1
         else:
             Fl = False 
-    return(A, temp, P)            
+    return(A, temp, P)   
