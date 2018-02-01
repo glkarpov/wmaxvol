3a6fbdd2a02e53c7a2e3da11872d93245d8526c5
Gleb Ryzhakov <gryzhakov@mail.ru>
2018-02-01 17:16:38 +0300

    more improvements

--- block_rect_maxvol.py
+++ block_rect_maxvol.py
@@ -25,64 +25,63 @@ class lining:
     def assemble(self):
         return np.dot(self.left, self.right_prod())
 
-# main func to form new coeff matrix    
+# main func to form new coeff matrix
 def rect_core(C, C_sigma, ndim):
     inv_block = la.inv(np.eye(ndim) + np.dot(C_sigma, C_sigma.conjugate().T))
     puzzle = lining(C_sigma,inv_block)
     U = np.hstack((np.eye(C.shape[1]) - puzzle.assemble(), puzzle.left_prod()))
     C_new = lining(C,U,inv = True).left_prod()
-    return C_new, puzzle.assemble()    
+    return C_new, puzzle.assemble()
 
-def form_permute(C, j, ind):
+def form_permute(C, j, ind): #  REMOVE THIS
     C[ind],C[j]=C[j],C[ind]
-    
-def mov_row(C, j, ind_x):
+
+
+def mov_row(C, j, ind_x): #  REMOVE THIS
     C[[ind_x,j],:] = C[[j,ind_x],:]
-    
+
+
 def cold_start(C, ndim):
-    k = C.shape[0] // ndim
     n = C.shape[0]
+    k = n // ndim
     values = []
     for i in range(0,k):
         CC_T = np.dot(C[i*ndim:i*ndim+ndim], C[i*ndim:i*ndim+ndim].conjugate().T)
         values.append((CC_T))
-    return values    
-    
+    return values
+
+
 def matrix_prep(A, ndim, l):
-    n,m = A.shape[0],A.shape[1]
-    B = np.zeros((n,m),dtype=float)
-    for j in range(0,l):
-        block = j * ndim
-        B[block + np.arange(ndim),:] = A[l * np.arange(ndim) + j,:]
-    return (B) 
+    return A[ np.arange(l*ndim).reshape(ndim, l).flatten(order='F') , :]
+
 
 def rect_block_maxvol_core(A_init, nder, Kmax, t = 0.05):
     ndim = nder + 1
-    M,n = A_init.shape[0], A_init.shape[1]
+    M, n = A_init.shape
     block_n = M // ndim # Whole amount of blocks in matrix A
     P = np.arange(M) # Permutation vector
     Fl = True
     Fl_cs = True
     ids_init = A_init[:n]
-    temp_init = np.dot(A_init,np.linalg.pinv(ids_init))
+    temp_init = np.dot(A_init, np.linalg.pinv(ids_init))
 
     A = np.copy(A_init)
     A_hat = np.copy(ids_init)
     C = np.copy(temp_init)
-    
+
     shape_index = n
     C_w = np.copy(C)
     CC_sigma = []
-    
+
     while (Fl == True) and (shape_index < Kmax):
-            
+
         if (Fl_cs == False):
             ind_array = la.det(np.eye(ndim) + CC_sigma)
             elem = np.argmax(np.abs(ind_array[(shape_index // ndim):])) + (shape_index // ndim)
             print elem
             if (ind_array[elem] > 1 + t):
                 CC_sigma[shape_index/ndim], CC_sigma[elem] = CC_sigma[elem], CC_sigma[shape_index/ndim]
-                for idx in range(ndim):                          
+                for idx in range(ndim):
                     form_permute(P,shape_index + idx,elem*ndim + idx)
                     mov_row(C_w,shape_index + idx,elem*ndim + idx)
                 C_new, line = rect_core(C_w,C_w[shape_index:shape_index + ndim],ndim)
@@ -91,7 +90,7 @@ def rect_block_maxvol_core(A_init, nder, Kmax, t = 0.05):
                 ### update list of CC_sigma
                 for k in range(block_n):
                     CC_sigma[k] = CC_sigma[k] - lining(C_w[k*ndim:ndim*(k+1)],line,inv=True).assemble()
-                C_w = C_new     
+                C_w = C_new
                 shape_index += ndim
             else:
                 print ('elements not found')
@@ -104,7 +103,7 @@ def rect_block_maxvol_core(A_init, nder, Kmax, t = 0.05):
             print elem
             if (ind_array[elem] > 1 + t):
                 CC_sigma[shape_index/ndim], CC_sigma[elem] = CC_sigma[elem], CC_sigma[shape_index/ndim]
-                for idx in range(ndim):                          
+                for idx in range(ndim):
                     form_permute(P,shape_index + idx,elem*ndim + idx)
                     mov_row(C_w,shape_index + idx,elem*ndim + idx)
             else:
@@ -117,10 +116,10 @@ def rect_block_maxvol_core(A_init, nder, Kmax, t = 0.05):
             for k in range(block_n):
                 CC_sigma[k] = CC_sigma[k] - np.dot(C_w[k*ndim:ndim*(k+1)], np.dot(line, C_w[k*ndim:ndim*(k+1)].conjugate().T))
             C_w = C_new 
-            Fl_cs = False      
+            Fl_cs = False
             
 
-    return(C_w, CC_sigma, P)     
+    return (C_w, CC_sigma, P)
     
 def rect_block_maxvol(A, nder, Kmax, max_iters, rect_tol = 0.05, tol = 0.0, debug = False, ext_debug = False):
     assert (A.shape[1] % (nder+1) == 0)
@@ -165,49 +164,49 @@ def test(A,x,x_test, nder, col_expansion, N_rows, function):
     plt.grid(True)
     fn = 'func={}_d={}_num={}_nder={}.pdf'.format(function.__name__, N_column, N_rows, nder)
     plt.savefig(fn)
-    return (error)
-    
+    return error
+
 def approximant(nder, coef):
     components = symbols(' '.join(['x' + str(comp_iter) for comp_iter in xrange(nder)]))
     sym_monoms = GenMat(coef.shape[0], np.array([components]),poly=cheb, debug=False,pow_p=1,ToGenDiff=False)
     evaluate = np.dot(sym_monoms[0], coef)
     evaluate = simplify(evaluate)
     res = utilities.lambdify(components, evaluate, 'numpy')
-    return (res)    
+    return res
 
 def test_points_gen(n_test, nder, distrib = 'random'):
     if distrib == 'random' :
-        x_test = 2*np.random.rand(n_test,nder) - 1
+        x_test = 2*np.random.rand(n_test, nder) - 1
     if distrib == 'LHS' :
         x_test = lhs(nder, samples=n_test)
-    return (x_test)    
+    return x_test
      
 def error_est(origin_func, approx, points):
-    error = la.norm(origin_func(*points.T) - approx(*points.T),np.inf) / la.norm(origin_func(*points.T), np.inf)
-    return (error)
+    error = la.norm(origin_func(*points.T) - approx(*points.T), np.inf) / la.norm(origin_func(*points.T), np.inf)
+    return error
 
 ### returns 2 values - function on domain, and block structured
 def gauss_sp(x,y):
     func = 2*exp(-((x**2)/2. + (y**2)/2.))
-    return (func)
+    return func
 
 def quadro_3(x,y,z):
-    return(2*((x**2)/2. + (y**2)/2. + (z**2)/2.))
+    return (2*((x**2)/2. + (y**2)/2. + (z**2)/2.))
 
 def many_dim_sp(x,y,z,a,b):
     func = sin(x+y+z) + a*b
-    return (func)
+    return func
 
 def linear_sp(x,y):
     return (5*x + x**2 + y**2)
 
-gauss, _ = symb_to_func(gauss_sp, 2)
-many_dim, _ = symb_to_func(many_dim_sp,5)
-linear,_ = symb_to_func(linear_sp,2)
+gauss     = symb_to_func(gauss_sp,    2, False)
+many_dim  = symb_to_func(many_dim_sp, 5, False)
+linear    = symb_to_func(linear_sp,   2, False)
 
-gauss.diff = [FindDiff(gauss_sp, 2, i, False) for i in range(1,3)]
+gauss.diff    = [FindDiff(gauss_sp, 2, i, False) for i in range(1,3)]
 many_dim.diff = [FindDiff(many_dim_sp, 5, i, False) for i in range(1,6)]
-linear.diff = [FindDiff(linear, 2, i, False) for i in range(1,3)]
+linear.diff   = [FindDiff(linear, 2, i, False) for i in range(1,3)]
 quadro_3.diff = [FindDiff(quadro_3, 3, i, False) for i in range(1,4)]
 
 def RHS(function, nder, points):
