889504cd9be966977e1230b53b72d31fb309aa10
glebsg <gleb.karpov@skolkovotech.ru>
2019-01-17 14:45:19 +0300

    Major improvement of BMaxvol; a little bit reduced entropy

--- mva_test.py
+++ mva_test.py
@@ -12,6 +12,12 @@ from scipy.spatial.distance import pdist
 from sobol_lib import *
 from block_rect_maxvol import *
 from gen_points import *
+import sys
+import matplotlib.pyplot as plt
+import matplotlib
+from mpl_toolkits.mplot3d import Axes3D
+import re
+from matplotlib import cm
 
 @jit
 def NumOfClusters(pnts, tol=0.005, full=True):
@@ -228,6 +234,9 @@ def NumOfClusters(pnts, tol=0.005, full=True):
 def gauss_sp(x,y):
     return 2*sp.exp(-((x**2)/2. + (y**2)/2.))
 
+def gauss_double_sp(x,y):
+    return 2*sp.exp(-(((x+0.9)**2)/2. + ((y-0.4)**2)/2.)) + 2*sp.exp(-(((x-0.9)**2)/2. + ((y+0.4)**2)/2.))
+
 def sincos_sp(x,y):
     return (sp.sin((x**2)/2. - (y**2)/4. + 3) * sp.cos(2*x + 1 - sp.exp(y)))
 
@@ -280,6 +289,7 @@ def piston(M,S,V_0,k,P_0,T_a,T_0):
     return C
 
 f_gauss      = symb_to_func(gauss_sp,      2, True, False, name='Gauss')
+f_gauss_doubl= symb_to_func(gauss_double_sp,2,True, False, name='Gauss_doubl')
 f_sincos     = symb_to_func(sincos_sp,     2, True, False, name='Sincos')
 f_rosenbrock = symb_to_func(rosenbrock_sp, 2, True, False, name='Rosenbrock')
 f_roots      = symb_to_func(roots_sp,      2, True, False, name='Roots')
@@ -290,6 +300,7 @@ f_holsclaw   = symb_to_func(holsclaw_sp,   2, True, False, name='Holsclaw')
 f_ellipse    = symb_to_func(ellipse_sp,    2, True, False, name='Ellipse')
 
 f_gauss.diff      = MakeDiffs(gauss_sp, 2)
+f_gauss_doubl.diff= MakeDiffs(gauss_double_sp, 2)
 f_sincos.diff     = MakeDiffs(sincos_sp, 2)
 f_rosenbrock.diff = MakeDiffs(rosenbrock_sp, 2)
 f_roots.diff      = MakeDiffs(roots_sp, 2)
@@ -324,21 +335,108 @@ def test_bm(A, x, nder, col_expansion, N_rows, cut_radius = 0.15, to_save_pivs=T
     cut_piv = pivs[:N_rows]
     taken_indices = cut_piv[::(nder+1)] // (nder+1)
     
-    if nder == 2 and (fnpdf is not None or to_export_pdf):
-        l_bound = np.amin(x, 0)
-        u_bound = np.amax(x, 0)
-        delta = (u_bound - l_bound)/20.0
-        fig = plt.figure()
-        plt.xlim(l_bound[0] - delta[0], u_bound[0] + delta[0])
-        plt.ylim(l_bound[1] - delta[1], u_bound[1] + delta[1])
-        plt.plot(x[taken_indices, 0], x[taken_indices, 1], 'b^')
-        # plt.title("E = {}".format(error))
-        plt.grid(True)
-        if fnpdf is None:
-            # fnpdf = 'func={}_columns={}_rows={}_pnts={}.pdf'.format(function.__name__, N_column, N_rows, len(taken_indices))
-            fnpdf = 'columns={}_rows={}.pdf'.format(N_column, N_rows)
-        # print "Num of points = {}, saving to file {}".format(len(taken_indices), fnpdf)
-        plt.savefig(fnpdf)
-        plt.close(fig)
+    #if nder == 2 and (fnpdf is not None or to_export_pdf):
+        #l_bound = np.amin(x, 0)
+        #u_bound = np.amax(x, 0)
+        #delta = (u_bound - l_bound)/20.0
+        #fig = plt.figure()
+        #plt.xlim(l_bound[0] - delta[0], u_bound[0] + delta[0])
+        #plt.ylim(l_bound[1] - delta[1], u_bound[1] + delta[1])
+        #plt.plot(x[taken_indices, 0], x[taken_indices, 1], 'b^')
+        ##plt.title("E = {}".format(error))
+        #plt.grid(True)
+        #if fnpdf is None:
+            #fnpdf = 'func={}_columns={}_rows={}_pnts={}.pdf'.format(function.__name__, N_column, N_rows, len(taken_indices))
+            #fnpdf = 'columns={}_rows={}.pdf'.format(N_column, N_rows)
+        ##print("Num of points = {}, saving to file {}".format(len(taken_indices), fnpdf))
+        #plt.savefig(fnpdf)
+        #plt.close(fig)
 
     return taken_indices
+
+
+
+#------------------- Visualisation ----------
+
+def file_extraction(Filepath, new_extr = True):
+    if new_extr:
+        srch = re.compile(r'([\d\s]+)_Nrows=(\d+)_expans=(\d+)')
+        fnd = srch.findall(open(Filepath, 'r').read())
+        return tuple(np.array(i) for i in zip(*[(int(i1), int(i2), [int(p) for p in im1.strip().split(' ') if len(p) > 0])
+                                                for im1, i1, i2 in fnd]))
+    else:
+        srch = re.compile(r'([\d\s]+)_error=([\+\-\d\.eE]+)_Nrows=(\d+)_expans=(\d+)')
+        fnd = srch.findall(open(Filepath, 'r').read())
+        return tuple(np.array(i) for i in zip(*[(float(i0), int(i1), int(i2), [int(p) for p in im1.strip().split(' ') if len(p) > 0])
+                                                for im1, i0, i1, i2 in fnd]))
+
+
+def DataToMesh(error, N_row, N_col, *args):
+    row_s = sorted(list(set(N_row)))
+    col_s = sorted(list(set(N_col)))
+    data = {(N_row[i], N_col[i]) : e for i, e in enumerate(error)}
+    
+    res = np.empty((len(row_s), len(col_s)), dtype=float)
+    for i, r in enumerate(row_s):
+        for j, c in enumerate(col_s):
+            try:
+                res[i,j] = data[(r, c)]
+            except:
+                res[i,j] = np.nan
+    X, Y = np.meshgrid(row_s, col_s)
+    return res.T, X, Y
+
+
+def PlotError(fn, log_it=False):
+    error, N_row, N_col = DataToMesh(*file_extraction(fn))
+    if log_it:
+        error = np.log10(error)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(N_row, N_col, error, edgecolor='black', linewidth=0.5, cmap = cm.Spectral)
+    # ax.legend()
+    ax.set_xlabel('N_rows', fontsize=10)
+    ax.set_ylabel('N_columns')
+    plt.show()
+
+
+def PlotPoints(fn, row, col, x, to_save_fig=False, fn_out="func.pdf"):
+    _, N_row, N_col, idx = file_extraction(fn)
+
+    l_bound = np.amin(x, 0)
+    u_bound = np.amax(x, 0)
+    delta = (u_bound - l_bound)/20.0
+    plt.xlim(l_bound[0] - delta[0], u_bound[0] + delta[0])
+    plt.ylim(l_bound[1] - delta[1], u_bound[1] + delta[1])
+    plt.plot(x[idx, 0], x[idx, 1], 'b^')
+    # plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, borderaxespad=0.)
+    # plt.title("E = {}".format(error))
+    plt.grid(True)
+    if to_save_fig:
+        # fn = 'func={}_d={}_num={}_nder={}.pdf'.format(function.__name__, N_column, N_rows, nder)
+        plt.savefig(fn_out)
+
+
+def PlotError_3D(N_row, N_col, error_ext, log_it=False):
+    error, N_row, N_col = DataToMesh(error_ext, N_row, N_col)
+    
+    if log_it:
+        error = np.log10(error)
+    
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(N_row, N_col, error, edgecolor='black', linewidth=0.5, cmap = cm.Spectral)
+    # ax.legend()
+    ax.set_xlabel('N_rows', fontsize=10)
+    ax.set_ylabel('N_columns')
+    plt.show()
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        fn = sys.argv[1]
+    else:
+        fn = "func=Rosenbrock_poly=cheb.txt"
+
+    PlotError(fn, log_it=False)
