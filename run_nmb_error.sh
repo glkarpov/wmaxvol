628736459720853aa9fc396d908179f8339024d6
glebsg <gleb.karpov@skolkovotech.ru>
2022-11-24 21:42:47 +0300

    Delete files not connected with block maxvol experiments

--- run_nmb_error.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-EXE='python3 ./nmb_error.py'
-ndim=2
-experiment='designs_dim='$ndim'_aaa'
-func=f_sincos
-param_str="--ndim=$ndim --experiment=$experiment --func=$func"
-$EXE $param_str
